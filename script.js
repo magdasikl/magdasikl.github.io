e635901e6bab3b9025ac93429eb44147d7c833ea
magdasikl <msiklucka@gmail.com>
2018-08-21 22:56:14 +0200

    Poprawa frontendull

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://damp-hamlet-16154.herokuapp.com/v1/task/';
+  const apiRoot = 'https://damp-hamlet-16154.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://damp-hamlet-16154.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
